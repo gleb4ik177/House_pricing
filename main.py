c905f339c25e140cf83f454b3ac47a0ec513c688
gleb4ik177 <64707220+gleb4ik177@users.noreply.github.com>
2024-06-04 11:27:13 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+def main():
+    print("This is main function!")
+
+    def test_func(params, second_params, third_params, fourth_params):
+        return None
+
+
+if __name__ == "__main__":
+    main()
