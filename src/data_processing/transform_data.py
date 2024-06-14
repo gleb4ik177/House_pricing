f39052ef73d559ab919eeca1c7e70f261c5882ec
Gleb <glebolymp@gmail.com>
2024-06-14 18:51:39 +0300

    Add module for transform the data

---
+++ src/data_processing/transform_data.py
@@ -0,0 +1,14 @@
+import pandas as pd
+
+def transform(df:pd.DataFrame):
+    """ Трансформирует данные (удаляет ненужные столбцы)
+
+            Parameters
+            ----------
+            df : pd.DataFrame
+                Датафрейм, с которым работаем
+            Returns
+            -------
+            pd.DataFrame
+                Трансформированные датафрейм """
+    return df.drop(['Id', 'PoolQC', 'MiscFeature', 'Alley', 'Fence', 'MasVnrType', 'FireplaceQu', 'LotFrontage'], axis=1)
